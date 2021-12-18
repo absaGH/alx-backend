447a33b2256e303456b2d14520066e25091b3ab1
absaGH <absa3852@gmail.com>
2021-12-18 11:50:37 +0000

    helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+from typing import Tuple
 '''module to find index of pagination'''
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''function that returns start index & range of pagination'''
-    sind = 0
-    eind = 0
+    sind: int = 0
+    eind: int = 0
 
     if page != 1:
         sind = (page - 1) * page_size
