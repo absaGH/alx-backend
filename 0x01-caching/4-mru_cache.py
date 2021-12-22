f2fb49a79cb41775397153ee736cb951a67fa486
absaGH <absa3852@gmail.com>
2021-12-22 06:51:25 +0000

    MRU caching policy

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -20,14 +20,14 @@ class MRUCache(BaseCaching):
         if key is not None and item is not None:
             if key in self.cache_data:
                 self.cache_data[key] = item
-                self.keydict[key] = self.keydict[max(
-                    self.keydict, key=self.keydict.get)] + 1
+                MRUkey = max(self.keydict, key=self.keydict.get)
+                self.keydict[key] = self.keydict[MRUkey] + 1
             elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 removed = max(self.keydict, key=self.keydict.get)
                 self.cache_data.pop(removed)
                 self.keydict.pop(removed)
-                self.keydict[key] = self.keydict[max(
-                    self.keydict, key=self.keydict.get)] + 1
+                MRUkey = max(self.keydict, key=self.keydict.get)
+                self.keydict[key] = self.keydict[MRUkey] + 1
                 self.cache_data[key] = item
                 print('DISCARD:', str(removed))
             else:
@@ -36,16 +36,16 @@ class MRUCache(BaseCaching):
                 if not has_item:
                     self.keydict[key] = 0
                 else:
-                    self.keydict[key] = self.keydict[max(
-                        self.keydict, key=self.keydict.get)] + 1
+                    MRUkey = max(self.keydict, key=self.keydict.get)
+                    self.keydict[key] = self.keydict[MRUkey] + 1
 
     def get(self, key):
         '''implementation of method of BaseCaching
            it get an item by key
         '''
         if key is not None and key in self.cache_data:
-            self.keydict[key] = self.keydict[max(
-                self.keydict, key=self.keydict.get)] + 1
+            MRUkey = max(self.keydict, key=self.keydict.get)
+            self.keydict[key] = self.keydict[MRUkey] + 1
             return self.cache_data[key]
         else:
             return None
