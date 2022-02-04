c196a80c4acee9cc4824211943fef14069499c99
absaGH <absa3852@gmail.com>
2022-02-04 09:59:34 +0000

    job creator and processor using kue

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,24 +1,13 @@
-import redis from 'redis';
 import kue from 'kue';
-
-const client = redis.createClient();
 
 const jobInfo = {
     phoneNumber: '4159518782',
     message: 'This is the code 4321 to verify your account'
 };
 
-const push_notification_code = kue.createQueue();
+const q = kue.createQueue();
 
-(()=> {
-
-  client.on('error', (err) => console.log('Redis client not connected to the server: ', err));
-
-  client.on('connect', () => console.log('Redis client connected to the server'));
-
-})();
-
-const job = push_notification_code.create(jobInfo).save( (err) => {
+const job = q.create('push_notification_code', jobInfo).save( (err) => {
    if( !err ) console.log( `Notification job created: ${job.id}` );
 });
 
